93ebb0225ed90a13a5971de8939e6a0bb26bc851
Eren Güven <erenguven0@gmail.com>
2013-04-28 14:28:05 +0300

    add django default login/logout and frontend changes for them

--- int_todo/urls.py
+++ int_todo/urls.py
@@ -1,6 +1,7 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
@@ -11,8 +12,12 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # Delivery of the static client
-    url(r'^$', direct_to_template,
+    url(r'^$', login_required(direct_to_template),
         {'template': 'todo_client/index.html'}, name='client'),
+    # built-in django login/logout
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/'}),
 
     # REST API endpoints
     url(r'^rest/todo/', include('todo_api_piston.urls')),
