0eb919cfeac35b23c3ad19e62ef051907580b0c3
Eren Güven <erenguven0@gmail.com>
2013-04-28 13:30:19 +0300

    add test client with login, update tests to add TodoItems with user

--- todo/tests.py
+++ todo/tests.py
@@ -6,10 +6,11 @@ Replace this with more appropriate tests for your application.
 """
 import json
 
+from django.core.management import call_command
 from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
 
-from django.test import TestCase
-from todo.models import TodoItem
+from todo.models import TodoItem, User
 
 
 class TodoItemTest(TestCase):
@@ -36,6 +37,23 @@ class TodoItemTest(TestCase):
              'done': False})
 
 class RestApiTests(TestCase):
+    def setUp(self):
+        '''Create user and login with client'''
+        self.client = Client(enforce_csrf_checks=True)
+        credentials_1 = {'username': 'user1', 'password': 'pass1'}
+        self.assertFalse(self.client.login(**credentials_1))
+        call_command('adduser', *credentials_1.values())        
+        self.assertTrue(self.client.login(**credentials_1))
+        self.user = User.objects.get(username='user1')
+
+    def test_nologin(self):
+        '''Test 403 responses from REST api without authentication'''
+        response = Client().get(reverse('todo_api:handle_rest_call'))
+        self.assertEqual(response['Content-Type'], 'application/json')
+        self.assertEqual(
+            {'success': False, 'error': 'Forbidden'},
+            json.loads(response.content))
+
     def test_base(self):
         '''Test bad request and empty GET response'''
         response = self.client.get(reverse('todo_api:handle_rest_call'))
@@ -69,14 +87,14 @@ class RestApiTests(TestCase):
             reverse('todo_api:handle_rest_call_id', args=('1',)),
             data='{"pk": 1}', content_type='application/json')
         self.assertEqual(404, not_found_response.status_code)
-        item = TodoItem(title='foo', order=1, done=False)
-        item.save()
+        item = self.user.todoitem_set.create(title='foo', order=1, done=False)
         put_data = item.to_json()
         put_data['done'] = True
         put_data_json = json.dumps(put_data)
         response = self.client.put(
             reverse('todo_api:handle_rest_call_id', args=('1',)),
             data=put_data_json, content_type='application/json')
+        self.assertEqual({'success': True}, json.loads(response.content))
         item = TodoItem.objects.get(pk=1)
         response = self.client.get(
             reverse('todo_api:handle_rest_call_id', args=('1',)))
@@ -91,10 +109,10 @@ class RestApiTests(TestCase):
         bad_response = self.client.delete(
             reverse('todo_api:handle_rest_call'))
         self.assertEqual(400, bad_response.status_code)
-        item = TodoItem(title='foo', order=42, done=False)
-        item.save()
+        item = self.user.todoitem_set.create(title='foo', order=42, done=False)
         response = self.client.delete(
             reverse('todo_api:handle_rest_call_id', args=('1')))
+        self.assertNotEqual(404, response.status_code)
         self.assertEqual(0, TodoItem.objects.count())
         response = self.client.get(reverse('todo_api:handle_rest_call'))
         self.assertEqual('[]', response.content)
